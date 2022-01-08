e6d0624a3eef689416a57176316c98f19faf9c96
kuldeep singh chouhan <96857519+kuldeepsinghchouha@users.noreply.github.com>
2022-01-08 15:25:32 +0530

    Add files via upload

---
+++ Example2.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the value of the  first number"))
+y=int(input("Enter the value of the  second number"))
+z= x+y
+print("The result of addition of ",x," and ",y," = ",z)
+print("The result of addition of "+str(x)+" and "+str(y)+" = "+str(z))
+print("The result of addition of %d and %d = %d " %(x,y,z))
+print("The result of addition of {0} and {-1} = {2}".format(x,y,z))
+
+
